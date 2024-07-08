da2a9ba6d26854d0200306d3d8dc4eab03ba6f03
ArtemDevelop <artem.podovilnikov@gmail.com>
2024-07-08 11:08:09 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 
-let numberOfFilms;
 
-numberOfFilms = prompt("How many films do you watch?","");
-console.log(numberOfFilms);
+
+const numberOfFilms = +prompt("How many films did you watch?","");
+
+const persomalMovieDB={
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+const a = prompt("One of your films witch did you watch last time?",""),
+      b = prompt("What mark do you give it?",""),
+      c = prompt("One of your films witch did you watch last time?",""),
+      d = prompt("What mark do you give it?","");
+
+      persomalMovieDB.movies[a] = b;
+      persomalMovieDB.movies[c] = d;
+
+console.log(persomalMovieDB);
