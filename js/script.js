4a84fb8cc659b15d12eba45b3a6c0238c6f90161
ArtemDevelop <artem.podovilnikov@gmail.com>
2024-07-18 20:49:51 +0300

    cycles

--- js/script.js
+++ js/script.js
@@ -1,22 +1,72 @@
+"use strict"
+function firstTask(){
+    for(let i=5;i<=10;i++){
+        console.log(i);
+    }
+}
+function secondTask(){
+    for(let i=20;i>=10;i--){
+        if(i===13){
+            break;
+        }
+        console.log(i);
+    }
+}
+
+function thirdTask(){
+    for(let i=2;i<=10;i++){  
+        if(i%2){
+            continue;
+        } 
+        console.log(i);
+    }
+}
+
+function fourthTask(){
+    let i=2;
+    // for (let i = 2; i <= 16; i++) {
+    //     if (i % 2 === 0) {
+    //         continue;
+    //     } else {
+    //         console.log(i);
+    //     }
+    // }
+    while(i <= 14){
+        i++;
+        if(i%2===0){
+            continue;
+        }else{
+            console.log(i);
+        }
+    }
+}
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+    for(let i=5;i<=10;i++){
+        arrayOfNumbers[i-5]=i;
+    }
+    console.log(arrayOfNumbers);
+}
+
+
+//adventure tasks
+
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    // Пишем решение вот тут
+    
+    
+    // Не трогаем
+    return result;
+}
 
 
 
-const numberOfFilms = +prompt("How many films did you watch?","");
 
-const persomalMovieDB={
-    count:numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    privat: false
-};
 
-const a = prompt("One of your films witch did you watch last time?",""),
-      b = prompt("What mark do you give it?",""),
-      c = prompt("One of your films witch did you watch last time?",""),
-      d = prompt("What mark do you give it?","");
 
-      persomalMovieDB.movies[a] = b;
-      persomalMovieDB.movies[c] = d;
 
-console.log(persomalMovieDB);
